fb3e3e2c3e795638168f5a239eba5f3c902689f3
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2021-04-02 14:20:53 -0500

    Le agregue la funcionalidad de abrir navegacion con el menú de hamburguesa

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 // Elements
 const menuButtonEl = document.querySelector('#mobile-menu');
 const menuBarEl = document.querySelector('#menu-bar');
+const navListEl = document.querySelector('#nav__list');
 
 menuButtonEl.addEventListener('click', function () {
   menuBarEl.classList.toggle('nav__menu-bar--active');
+  navListEl.classList.toggle('nav__list--active');
 });
