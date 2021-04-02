0117731f74fb285d07ca8e285249d8e78e45cb6b
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2021-04-02 15:15:14 -0500

    Agregando funcionalidad scroll navigation al proyecto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,47 @@
 'use strict';
 
-// Elements
-const menuButtonEl = document.querySelector('#mobile-menu');
-const menuBarEl = document.querySelector('#menu-bar');
-const navListEl = document.querySelector('#nav__list');
+$(document).ready(function () {
+  /* Navigation scroll */
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
 
-menuButtonEl.addEventListener('click', function () {
-  menuBarEl.classList.toggle('nav__menu-bar--active');
-  navListEl.classList.toggle('nav__list--active');
+  /* Scroll on buttons */
+  $('.js--scroll-platos').click(function () {
+    $('html, body').animate({ scrollTop: $('#platos').offset().top }, 1000);
+  });
+
+  /* Mobile Navigation */
+  $('#mobile-menu').click(function () {
+    const mainNav = $('#nav__list');
+    const icon = $('#menu-bar');
+
+    mainNav.slideToggle(200);
+
+    if (icon.hasClass('nav__menu-bar--active')) {
+      icon.removeClass('nav__menu-bar--active');
+    } else {
+      icon.addClass('nav__menu-bar--active');
+    }
+  });
 });
